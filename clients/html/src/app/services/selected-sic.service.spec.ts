18e2ba3a04abcf7d98abd2e4b78c5b007f49c714
Chaz Haskins <chaz.haskins@gmail.com>
2019-10-07 07:35:33 -0400

    sic feature update

---
+++ clients/html/src/app/services/selected-sic.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SelectedSicService } from './selected-sic.service';
+
+describe('SelectedSicService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: SelectedSicService = TestBed.get(SelectedSicService);
+    expect(service).toBeTruthy();
+  });
+});
