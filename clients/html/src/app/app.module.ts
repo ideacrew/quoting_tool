f665fda6c16d23a1627a9f13d8c03c3497fdd20c
Chaz Haskins <chaz.haskins@gmail.com>
2019-11-22 16:05:47 -0500

    Removed unused components

--- clients/html/src/app/app.module.ts
+++ clients/html/src/app/app.module.ts
@@ -13,20 +13,9 @@ import { NgbDateAdapter, NgbDateStruct, NgbDateNativeAdapter } from '@ng-bootstr
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { TreeviewModule } from 'ngx-treeview';
 
-import { FullComponent } from './layouts/full/full.component';
-import { BlankComponent } from './layouts/blank/blank.component';
-
-import { NavigationComponent } from './shared/header-navigation/navigation.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
-
 import { routes } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SpinnerComponent } from './shared/spinner.component';
 
-import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
-import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
-import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 import { EmployerDetailsComponent } from './employer-details/employer-details.component';
 import { EmployerDetailsHealthComponent } from './employer-details/employer-details-health/employer-details-health.component';
 import { EmployerDetailsDentalComponent } from './employer-details/employer-details-dental/employer-details-dental.component';
@@ -39,22 +28,10 @@ import { PlanFilterPipe } from './plan-filter/plan-filter.pipe';
 import { OrderByPipe } from './plan-filter/order-by.pipe';
 import { CoverageTypePipe } from './employer-details/coverage-type.pipe';
 
-const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
-  suppressScrollX: true,
-  wheelSpeed: 1,
-  wheelPropagation: true,
-  minScrollbarLength: 20
-};
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpinnerComponent,
-    FullComponent,
-    BlankComponent,
-    NavigationComponent,
-    BreadcrumbComponent,
-    SidebarComponent,
     EmployerDetailsComponent,
     EmployerDetailsHealthComponent,
     EmployerDetailsDentalComponent,
@@ -77,15 +54,10 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     HttpClientModule,
     NgbModule,
     RouterModule.forRoot(routes),
-    PerfectScrollbarModule,
     NgxDatatableModule,
     TreeviewModule.forRoot()
   ],
   providers: [
-    {
-      provide: PERFECT_SCROLLBAR_CONFIG,
-      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
-    },
     { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }
   ],
   bootstrap: [AppComponent]
