7a1cf391100ec636f50259791af98dd5db1e6c5d
vineel <sonnathivinil@gmail.com>
2020-02-10 08:12:09 -0500

    Use values from registry gem

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -13,21 +13,20 @@ class Api::V1::EmployeesController < ApplicationController
   end
 
   def start_on_dates
-    date = Date.today
-    minimum_length = 5
-    open_enrollment_end_on_day = 200
-    # open_enrollment_end_on_day - minimum_length
+    current_date = Date.today
+    minimum_length = Registry.resolve "aca_shop_market.open_enrollment.minimum_length_days"
+    open_enrollment_end_on_day = Registry.resolve "aca_shop_market.open_enrollment.monthly_end_on"
 
     minimum_day = open_enrollment_end_on_day - minimum_length
     minimum_day.positive? ? minimum_day : 1
 
-    start_on =  if date.day > minimum_day
-                  date.beginning_of_month + 2.months
+    start_on =  if current_date.day > minimum_day
+                  current_date.beginning_of_month + Registry.resolve("aca_shop_market.open_enrollment.maximum_length_months").months
                 else
-                  date.prev_month.beginning_of_month + 2.months
+                  current_date.prev_month.beginning_of_month + Registry.resolve("aca_shop_market.open_enrollment.maximum_length_months").months
                 end
 
-    end_on = date - (-2.months)
+    end_on = current_date - (Registry.resolve("aca_shop_market.initial_application.earliest_start_prior_to_effective_on_months").months)
     dates = (start_on..end_on).select {|t| t == t.beginning_of_month}.map {|a| a.to_s}.map{ |a| a.gsub!("-","/")}
     render json: { dates: dates }
   end
