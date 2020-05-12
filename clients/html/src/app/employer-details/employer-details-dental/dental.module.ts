2584ad237ca3e111ff167ac78a2358789d381d5b
nisanth <nisanth.yaganti@gmail.com>
2020-05-12 14:49:31 -0400

    setting child routes in dental module

--- clients/html/src/app/employer-details/employer-details-dental/dental.module.ts
+++ clients/html/src/app/employer-details/employer-details-dental/dental.module.ts
@@ -3,10 +3,20 @@ import { CommonModule } from '@angular/common';
 import { NgbDateAdapter, NgbDateNativeAdapter, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EmployerDetailsDentalComponent } from './employer-details-dental.component';
 import { SharedModule } from '../../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [EmployerDetailsDentalComponent],
-  imports: [CommonModule, NgbModule, SharedModule],
+  imports: [
+    CommonModule, NgbModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: EmployerDetailsDentalComponent
+      }
+    ]),
+    SharedModule
+  ],
   exports: [EmployerDetailsDentalComponent]
   // providers: [{ provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }]
 })
