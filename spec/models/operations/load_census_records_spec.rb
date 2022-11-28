a50df74c9243d29dcbcf0ac3a7d766e982b178ff
RTH_RoR <rohith.rubyrails@gmail.com>
2022-11-28 11:40:42 -0500

    fixed rubocop offenses

--- spec/models/operations/load_census_records_spec.rb
+++ spec/models/operations/load_census_records_spec.rb
@@ -3,11 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Operations::LoadCensusRecords, type: :transaction do
-  input = ActionDispatch::Http::UploadedFile.new({
-    :filename => 'roster_upload_template.xlsx',
-    :type => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    :tempfile => File.new("#{Rails.root}/spec/test_data/roster_upload_template.xlsx")
-  })
+  input = ActionDispatch::Http::UploadedFile.new(filename: 'roster.xlsx', type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', tempfile: File.new("#{Rails.root}/spec/test_data/roster.xlsx"))
+
   let!(:subject) { Operations::LoadCensusRecords.new.call(input) }
 
   context 'succesful' do
