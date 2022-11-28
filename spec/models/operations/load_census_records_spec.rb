db85d20b482c34680a83c038280b03f9dc3df776
RTH_RoR <rohith.rubyrails@gmail.com>
2022-11-28 11:06:06 -0500

    added spec for load_census data

--- spec/models/operations/load_census_records_spec.rb
+++ spec/models/operations/load_census_records_spec.rb
@@ -3,15 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Operations::LoadCensusRecords, type: :transaction do
-  let!(:county_zip) { FactoryBot.create(:county_zip, zip: '12345', county_name: 'County 1') }
-  let!(:subject) { Operations::LoadCensusRecords.new.call(file) }
+  input = ActionDispatch::Http::UploadedFile.new({
+    :filename => 'roster_upload_template.xlsx',
+    :type => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    :tempfile => File.new("#{Rails.root}/spec/test_data/roster_upload_template.xlsx")
+  })
+  let!(:subject) { Operations::LoadCensusRecords.new.call(input) }
 
   context 'succesful' do
-    let(:file) { File.join(Rails.root, 'spec/test_data/roster_upload_template.xlsx') }
-
     it 'should be success' do
       expect(subject.success?).to eq true
     end
-
   end
 end
