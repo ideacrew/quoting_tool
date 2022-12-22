a4cce112bcceac64b799cfbfe438a9d02ff05d00
RTH_RoR <rohith.rubyrails@gmail.com>
2022-12-22 13:06:27 -0500

    added registry for benefit model and find my doctor links

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -36,8 +36,17 @@ module Api
         dates_rates_hash = rates_for?(start_on..end_on)
         dates = dates_rates_hash.collect { |k, v| k.to_date.to_s.gsub!('-', '/') if v }.compact
         is_sic_codes_enabled = QuotingToolRegistry.feature_enabled?(:sic_codes)
+        is_benefit_model_enabled = QuotingToolRegistry.feature_enabled?(:benefit_model)
+        is_find_my_doctor_enabled = QuotingToolRegistry.feature_enabled?(:find_my_doctor)
 
-        render json: { dates: dates, is_late_rate: !dates_rates_hash.values.all?, is_sic_codes_enabled: is_sic_codes_enabled }
+        result = {
+          dates: dates,
+          is_late_rate: !dates_rates_hash.values.all?,
+          is_sic_codes_enabled: is_sic_codes_enabled,
+          is_benefit_model_enabled: is_benefit_model_enabled,
+          is_find_my_doctor_enabled: is_find_my_doctor_enabled
+        }
+        render json: result
       end
 
       private
