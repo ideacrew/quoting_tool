9682d9393976c81b719de03d71d2e88eb2cb80c3
Nisanth Yaganti <yagantinisanth@gmail.com>
2022-12-30 09:11:49 -0700

    fixing bucket name, clean up

--- lib/tasks/upload_sbc.rb
+++ lib/tasks/upload_sbc.rb
@@ -1,4 +1,6 @@
-csv_path = 'db/seedfiles/plan_xmls/me/sbcs/2023/2023_sbcs.csv'
-dir_path = 'db/seedfiles/plan_xmls/me/sbcs/2023/'
+year = ENV['sbc_year']
+csv_path = "db/seedfiles/plan_xmls/me/sbcs/#{year}/#{year}_sbcs.csv"
+dir_path = "db/seedfiles/plan_xmls/me/sbcs/#{year}/"
+
 sbc_processor = ::Services::SbcProcessor.new(csv_path, dir_path)
 sbc_processor.run
