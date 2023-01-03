16164944856b4368a57d5d82d4efc7d8b0ba9508
Nisanth Yaganti <yagantinisanth@gmail.com>
2023-01-03 09:01:45 -0700

    removing app name from s3 class (#46)
    * removing app name from s3 class
    
    * spec fix
    
    * setting bucket name through env variable

--- app/models/aws/s3_storage.rb
+++ app/models/aws/s3_storage.rb
@@ -11,8 +11,8 @@ module Aws
 
     # If success, return URI which has the s3 bucket key
     # else return nil
-    def save(file_path, bucket_name, key = SecureRandom.uuid)
-      bucket_name = env_bucket_name(bucket_name)
+    def save(file_path, key = SecureRandom.uuid)
+      bucket_name = ENV['AEQT_SBC_BUCKET']
       uri = "urn:openhbx:terms:v1:file_storage:s3:bucket:#{bucket_name}##{key}"
 
       begin
@@ -26,57 +26,14 @@ module Aws
 
     # If success, return URI which has the s3 bucket key
     # else return nil
-    def self.save(file_path, bucket_name, key = SecureRandom.uuid)
-      Aws::S3Storage.new.save(file_path, bucket_name, key)
-    end
-
-    # The uri has information about the bucket name and key
-    # e.g. "urn:openhbx:terms:v1:file_storage:s3:bucket:#{bucket_name}##{key}"
-    # The returned object can be streamed by controller
-    def find(uri)
-      bucket_and_key = uri.split(':').last
-      bucket_name, key = bucket_and_key.split('#')
-      env_bucket_name = correct_env_bucket_name(bucket_name)
-      object = get_object(env_bucket_name, key)
-      read_object(object)
-    rescue StandardError
-      puts format('Error caused due to %<error_info_class>s', error_info_class: $ERROR_INFO.class) unless Rails.env.test?
-      raise
-    end
-
-    # The param uri is present in Document model. Document.identifier
-    # The uri has information about the bucket name and key
-    # e.g. "urn:openhbx:terms:v1:file_storage:s3:bucket:#{bucket_name}##{key}"
-    # The returned object can be streamed by controller
-    def self.find(uri)
-      Aws::S3Storage.new.find(uri)
+    def self.save(file_path, key = SecureRandom.uuid)
+      Aws::S3Storage.new.save(file_path, key)
     end
 
     private
 
-    def read_object(object)
-      object.get.body.read
-    end
-
     def get_object(bucket_name, key)
       @resource.bucket(bucket_name).object(key)
-    end
-
-    def correct_env_bucket_name(bucket_name)
-      bucket_name_segment = bucket_name.split('-')
-
-      return bucket_name if ENV_LIST.include?(bucket_name_segment.last) && bucket_name_segment.last == aws_env
-
-      bucket_name_segment[bucket_name_segment.length - 1] = aws_env
-      bucket_name_segment.join('-')
-    end
-
-    def aws_env
-      ENV['AWS_ENV'] || 'qa'
-    end
-
-    def env_bucket_name(bucket_name)
-      "#{QuotingToolRegistry[:quoting_tool_app].setting(:s3_prefix).item}-enroll-#{bucket_name}-#{aws_env}"
     end
 
     def setup
--- spec/models/aws/s3_storage_spec.rb
+++ spec/models/aws/s3_storage_spec.rb
@@ -9,16 +9,20 @@ describe Aws::S3Storage do
   let(:bucket_name) { 'bucket1' }
   let(:file_path) { File.dirname(__FILE__) }
   let(:key) { SecureRandom.uuid }
-  let(:uri) { "urn:openhbx:terms:v1:file_storage:s3:bucket:#{QuotingToolRegistry[:quoting_tool_app].setting(:s3_prefix).item}-enroll-#{bucket_name}-#{aws_env}##{key}" }
+  let(:uri) { "urn:openhbx:terms:v1:file_storage:s3:bucket:#{QuotingToolRegistry[:quoting_tool_app].setting(:s3_prefix).item}-aeqt-#{bucket_name}-#{aws_env}##{key}" }
   let(:invalid_url) { 'urn:openhbx:terms:v1:file_storage:s3:bucket:' }
   let(:file_content) { 'test content' }
+
+  before :each do
+    stub_const('ENV', ENV.to_hash.merge('AEQT_SBC_BUCKET' => 'cme-aeqt-bucket1-qa'))
+  end
 
   describe 'save()' do
     context 'successful upload with explicit key' do
       it 'return the URI of saved file' do
         allow(object).to receive(:upload_file).with(file_path, server_side_encryption: 'AES256').and_return(true)
         allow_any_instance_of(Aws::S3Storage).to receive(:get_object).and_return(object)
-        expect(subject.save(file_path, bucket_name, key)).to eq(uri)
+        expect(subject.save(file_path, key)).to eq(uri)
       end
     end
 
@@ -26,7 +30,7 @@ describe Aws::S3Storage do
       it 'return the URI of saved file' do
         allow(object).to receive(:upload_file).with(file_path, server_side_encryption: 'AES256').and_return(true)
         allow_any_instance_of(Aws::S3Storage).to receive(:get_object).and_return(object)
-        expect(subject.save(file_path, bucket_name)).to include('urn:openhbx:terms:v1:file_storage:s3:bucket:')
+        expect(subject.save(file_path)).to include('urn:openhbx:terms:v1:file_storage:s3:bucket:')
       end
     end
 
@@ -34,26 +38,7 @@ describe Aws::S3Storage do
       it 'returns nil' do
         allow(object).to receive(:upload_file).with(file_path, server_side_encryption: 'AES256').and_return(nil)
         allow_any_instance_of(Aws::S3Storage).to receive(:get_object).and_return(object)
-        expect(subject.save(file_path, bucket_name)).to be_nil
-      end
-    end
-  end
-
-  describe 'find()' do
-    context 'success' do
-      it 'returns the file contents' do
-        allow_any_instance_of(Aws::S3Storage).to receive(:get_object).and_return(object)
-        allow_any_instance_of(Aws::S3Storage).to receive(:read_object).with(object).and_return(file_content)
-        expect(subject.find(uri)).to eq(file_content)
-      end
-    end
-
-    context 'failure (invalid uri)' do
-      it 'returns nil' do
-        allow_any_instance_of(Aws::S3Storage).to receive(:get_object).with('qa', nil).and_raise(StandardError)
-        expect do
-          subject.find(invalid_url)
-        end.to raise_error(StandardError)
+        expect(subject.save(file_path)).to be_nil
       end
     end
   end
